315f596386e26974595f15f34dad930b37d08e15
Leynier Gutiérrez González <leynier41@gmail.com>
2021-12-25 13:34:28 -0500

    perf: use inheritance to improve our code base (#47)
    * fix: interpolations erros and other things reported by sourcery-ai
    
    * feat: use inheritance to improve the code base
    
    * fix: sourcery refactored
    
    * chore: update pre commit rules
    
    * fix: remove noqa F401 comments
    
    * fix: remove duplicate and unused imports in base_client.py
    
    * feat: use enum instance literals in base_request_builder.py
    
    * pref: cast session only once in __init__
    
    * pref: remove unnecesary cast
    
    * tests: update tests
    
    * chore: generate sync code
    
    * feat: add support for upsert
    
    * Rm cast from rpc in async client
    
    * Rm cast from rpc in sync client
    
    * Add table method as an alias for from_
    
    Co-authored-by: dreinon <67071425+dreinon@users.noreply.github.com>
    Co-authored-by: Dani Reinón <dani@dribo.es>

--- postgrest_py/_sync/client.py
+++ postgrest_py/_sync/client.py
@@ -1,16 +1,19 @@
-from typing import Dict, Optional, Union
+from typing import Dict, cast
 
 from deprecation import deprecated
-from httpx import BasicAuth, Response
+from httpx import Response
 
 from postgrest_py.__version__ import __version__
-from postgrest_py.config import DEFAULT_POSTGREST_CLIENT_HEADERS
+from postgrest_py.base_client import (
+    DEFAULT_POSTGREST_CLIENT_HEADERS,
+    BasePostgrestClient,
+)
 from postgrest_py.utils import SyncClient
 
 from .request_builder import SyncRequestBuilder
 
 
-class SyncPostgrestClient:
+class SyncPostgrestClient(BasePostgrestClient):
     """PostgREST client."""
 
     def __init__(
@@ -20,14 +23,17 @@ class SyncPostgrestClient:
         schema: str = "public",
         headers: Dict[str, str] = DEFAULT_POSTGREST_CLIENT_HEADERS,
     ) -> None:
-        headers = {
-            **headers,
-            "Accept-Profile": schema,
-            "Content-Profile": schema,
-        }
-        self.session = SyncClient(base_url=base_url, headers=headers)
+        BasePostgrestClient.__init__(self, base_url, schema=schema, headers=headers)
+        self.session = cast(SyncClient, self.session)
 
-    def __enter__(self):
+    def create_session(
+        self,
+        base_url: str,
+        headers: Dict[str, str],
+    ) -> SyncClient:
+        return SyncClient(base_url=base_url, headers=headers)
+
+    def __enter__(self) -> "SyncPostgrestClient":
         return self
 
     def __exit__(self, exc_type, exc, tb) -> None:
@@ -36,41 +42,17 @@ class SyncPostgrestClient:
     def aclose(self) -> None:
         self.session.aclose()
 
-    def auth(
-        self,
-        token: Optional[str],
-        *,
-        username: Union[str, bytes, None] = None,
-        password: Union[str, bytes] = "",
-    ):
-        """
-        Authenticate the client with either bearer token or basic authentication.
-
-        Raise `ValueError` if neither authentication scheme is provided.
-        Bearer token is preferred if both ones are provided.
-        """
-        if token:
-            self.session.headers["Authorization"] = f"Bearer {token}"
-        elif username:
-            self.session.auth = BasicAuth(username, password)
-        else:
-            raise ValueError(
-                "Neither bearer token or basic authentication scheme is provided"
-            )
-        return self
-
-    def schema(self, schema: str):
-        """Switch to another schema."""
-        self.session.headers.update({"Accept-Profile": schema, "Content-Profile": schema})
-        return self
-
     def from_(self, table: str) -> SyncRequestBuilder:
         """Perform a table operation."""
         return SyncRequestBuilder(self.session, f"/{table}")
+    
+    def table(self, table: str) -> SyncRequestBuilder:
+        """Alias to self.from_()."""
+        return self.from_(table)
 
-    @deprecated("0.2.0", "1.0.0", __version__, "Use PostgrestClient.from_() instead")
+    @deprecated("0.2.0", "1.0.0", __version__, "Use self.from_() instead")
     def from_table(self, table: str) -> SyncRequestBuilder:
-        """Alias to Self.from_()."""
+        """Alias to self.from_()."""
         return self.from_(table)
 
     def rpc(self, func: str, params: dict) -> Response:
---
+++ postgrest_py/base_request_builder.py
@@ -0,0 +1,232 @@
+from re import search
+from typing import Any, Dict, Iterable, Optional, Tuple, Union
+
+from httpx import Response
+
+from postgrest_py.constants import CountMethod, Filters, RequestMethod
+from postgrest_py.utils import (
+    AsyncClient,
+    SyncClient,
+    sanitize_param,
+    sanitize_pattern_param,
+)
+
+
+def pre_select(
+    session: Union[AsyncClient, SyncClient],
+    path: str,
+    *columns: str,
+    count: Optional[CountMethod] = None,
+) -> Tuple[RequestMethod, dict]:
+    if columns:
+        method = RequestMethod.GET
+        session.params = session.params.set("select", ",".join(columns))
+    else:
+        method = RequestMethod.HEAD
+    if count:
+        session.headers["Prefer"] = f"count={count}"
+    return method, {}
+
+
+def pre_insert(
+    session: Union[AsyncClient, SyncClient],
+    path: str,
+    json: dict,
+    *,
+    count: Optional[CountMethod] = None,
+    upsert=False,
+) -> Tuple[RequestMethod, dict]:
+    prefer_headers = ["return=representation"]
+    if count:
+        prefer_headers.append(f"count={count}")
+    if upsert:
+        prefer_headers.append("resolution=merge-duplicates")
+    session.headers["prefer"] = ",".join(prefer_headers)
+    return RequestMethod.POST, json
+
+
+def pre_upsert(
+    session: Union[AsyncClient, SyncClient],
+    path: str,
+    json: dict,
+    *,
+    count: Optional[CountMethod] = None,
+    ignore_duplicates=False,
+) -> Tuple[RequestMethod, dict]:
+    prefer_headers = ["return=representation"]
+    if count:
+        prefer_headers.append(f"count={count}")
+    resolution = "ignore" if ignore_duplicates else "merge"
+    prefer_headers.append(f"resolution={resolution}-duplicates")
+    session.headers["prefer"] = ",".join(prefer_headers)
+    return RequestMethod.POST, json
+
+
+def pre_update(
+    session: Union[AsyncClient, SyncClient],
+    path: str,
+    json: dict,
+    *,
+    count: Optional[CountMethod] = None,
+) -> Tuple[RequestMethod, dict]:
+    prefer_headers = ["return=representation"]
+    if count:
+        prefer_headers.append(f"count={count}")
+    session.headers["prefer"] = ",".join(prefer_headers)
+    return RequestMethod.PATCH, json
+
+
+def pre_delete(
+    session: Union[AsyncClient, SyncClient],
+    path: str,
+    *,
+    count: Optional[CountMethod] = None,
+) -> Tuple[RequestMethod, dict]:
+    prefer_headers = ["return=representation"]
+    if count:
+        prefer_headers.append(f"count={count}")
+    session.headers["prefer"] = ",".join(prefer_headers)
+    return RequestMethod.DELETE, {}
+
+
+def process_response(
+    session: Union[AsyncClient, SyncClient],
+    r: Response,
+) -> Tuple[Any, Optional[int]]:
+    count = None
+    prefer_header = session.headers.get("prefer")
+    if prefer_header:
+        pattern = f"count=({'|'.join([cm.value for cm in CountMethod])})"
+        count_header_match = search(pattern, prefer_header)
+        content_range_header = r.headers.get("content-range")
+        if count_header_match and content_range_header:
+            content_range = content_range_header.split("/")
+            if len(content_range) >= 2:
+                count = int(content_range[1])
+    return r.json(), count
+
+
+class BaseFilterRequestBuilder:
+    def __init__(self, session: Union[AsyncClient, SyncClient]):
+        self.session = session
+        self.negate_next = False
+
+    @property
+    def not_(self):
+        self.negate_next = True
+        return self
+
+    def filter(self, column: str, operator: str, criteria: str):
+        """Either filter in or filter out based on `self.negate_next.`"""
+        if self.negate_next is True:
+            self.negate_next = False
+            operator = f"{Filters.NOT}.{operator}"
+        key, val = sanitize_param(column), f"{operator}.{criteria}"
+        self.session.params = self.session.params.add(key, val)
+        return self
+
+    def eq(self, column: str, value: str):
+        return self.filter(column, Filters.EQ, sanitize_param(value))
+
+    def neq(self, column: str, value: str):
+        return self.filter(column, Filters.NEQ, sanitize_param(value))
+
+    def gt(self, column: str, value: str):
+        return self.filter(column, Filters.GT, sanitize_param(value))
+
+    def gte(self, column: str, value: str):
+        return self.filter(column, Filters.GTE, sanitize_param(value))
+
+    def lt(self, column: str, value: str):
+        return self.filter(column, Filters.LT, sanitize_param(value))
+
+    def lte(self, column: str, value: str):
+        return self.filter(column, Filters.LTE, sanitize_param(value))
+
+    def is_(self, column: str, value: str):
+        return self.filter(column, Filters.IS, sanitize_param(value))
+
+    def like(self, column: str, pattern: str):
+        return self.filter(column, Filters.LIKE, sanitize_pattern_param(pattern))
+
+    def ilike(self, column: str, pattern: str):
+        return self.filter(column, Filters.ILIKE, sanitize_pattern_param(pattern))
+
+    def fts(self, column: str, query: str):
+        return self.filter(column, Filters.FTS, sanitize_param(query))
+
+    def plfts(self, column: str, query: str):
+        return self.filter(column, Filters.PLFTS, sanitize_param(query))
+
+    def phfts(self, column: str, query: str):
+        return self.filter(column, Filters.PHFTS, sanitize_param(query))
+
+    def wfts(self, column: str, query: str):
+        return self.filter(column, Filters.WFTS, sanitize_param(query))
+
+    def in_(self, column: str, values: Iterable[str]):
+        values = map(sanitize_param, values)
+        values = ",".join(values)
+        return self.filter(column, Filters.IN, f"({values})")
+
+    def cs(self, column: str, values: Iterable[str]):
+        values = map(sanitize_param, values)
+        values = ",".join(values)
+        return self.filter(column, Filters.CS, f"{{{values}}}")
+
+    def cd(self, column: str, values: Iterable[str]):
+        values = map(sanitize_param, values)
+        values = ",".join(values)
+        return self.filter(column, Filters.CD, f"{{{values}}}")
+
+    def ov(self, column: str, values: Iterable[str]):
+        values = map(sanitize_param, values)
+        values = ",".join(values)
+        return self.filter(column, Filters.OV, f"{{{values}}}")
+
+    def sl(self, column: str, range: Tuple[int, int]):
+        return self.filter(column, Filters.SL, f"({range[0]},{range[1]})")
+
+    def sr(self, column: str, range: Tuple[int, int]):
+        return self.filter(column, Filters.SR, f"({range[0]},{range[1]})")
+
+    def nxl(self, column: str, range: Tuple[int, int]):
+        return self.filter(column, Filters.NXL, f"({range[0]},{range[1]})")
+
+    def nxr(self, column: str, range: Tuple[int, int]):
+        return self.filter(column, Filters.NXR, f"({range[0]},{range[1]})")
+
+    def adj(self, column: str, range: Tuple[int, int]):
+        return self.filter(column, Filters.ADJ, f"({range[0]},{range[1]})")
+
+    def match(self, query: Dict[str, Any]):
+        updated_query = None
+        for key in query:
+            value = query.get(key, "")
+            updated_query = self.eq(key, value)
+        return updated_query
+
+
+class BaseSelectRequestBuilder(BaseFilterRequestBuilder):
+    def __init__(self, session: Union[AsyncClient, SyncClient]):
+        BaseFilterRequestBuilder.__init__(self, session)
+
+    def order(self, column: str, *, desc=False, nullsfirst=False):
+        self.session.params[
+            "order"
+        ] = f"{column}{'.desc' if desc else ''}{'.nullsfirst' if nullsfirst else ''}"
+        return self
+
+    def limit(self, size: int, *, start=0):
+        self.session.headers["Range-Unit"] = "items"
+        self.session.headers["Range"] = f"{start}-{start + size - 1}"
+        return self
+
+    def range(self, start: int, end: int):
+        self.session.headers["Range-Unit"] = "items"
+        self.session.headers["Range"] = f"{start}-{end - 1}"
+        return self
+
+    def single(self):
+        self.session.headers["Accept"] = "application/vnd.pgrst.object+json"
+        return self
