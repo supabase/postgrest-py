c8153eaced21343e166ae304e4bcbf821a99dd2a
Leynier Gutiérrez González <leynier41@gmail.com>
2021-12-25 07:19:23 +0000

    fix: remove noqa F401 comments

--- postgrest_py/__init__.py
+++ postgrest_py/__init__.py
@@ -1,13 +1,17 @@
-from postgrest_py._async.client import AsyncPostgrestClient  # noqa: F401
-from postgrest_py._async.request_builder import AsyncFilterRequestBuilder  # noqa: F401
-from postgrest_py._async.request_builder import AsyncQueryRequestBuilder  # noqa: F401
-from postgrest_py._async.request_builder import AsyncRequestBuilder  # noqa: F401
-from postgrest_py._async.request_builder import AsyncSelectRequestBuilder  # noqa: F401
-from postgrest_py._sync.client import SyncPostgrestClient  # noqa: F401
-from postgrest_py._sync.request_builder import SyncFilterRequestBuilder  # noqa: F401
-from postgrest_py._sync.request_builder import SyncQueryRequestBuilder  # noqa: F401
-from postgrest_py._sync.request_builder import SyncRequestBuilder  # noqa: F401
-from postgrest_py._sync.request_builder import SyncSelectRequestBuilder  # noqa: F401
-from postgrest_py.base_client import DEFAULT_POSTGREST_CLIENT_HEADERS  # noqa: F401
-from postgrest_py.deprecated_client import Client, PostgrestClient  # noqa: F401
-from postgrest_py.deprecated_get_request_builder import GetRequestBuilder  # noqa: F401
+from postgrest_py._async.client import AsyncPostgrestClient
+from postgrest_py._async.request_builder import (
+    AsyncFilterRequestBuilder,
+    AsyncQueryRequestBuilder,
+    AsyncRequestBuilder,
+    AsyncSelectRequestBuilder,
+)
+from postgrest_py._sync.client import SyncPostgrestClient
+from postgrest_py._sync.request_builder import (
+    SyncFilterRequestBuilder,
+    SyncQueryRequestBuilder,
+    SyncRequestBuilder,
+    SyncSelectRequestBuilder,
+)
+from postgrest_py.base_client import DEFAULT_POSTGREST_CLIENT_HEADERS
+from postgrest_py.deprecated_client import Client, PostgrestClient
+from postgrest_py.deprecated_get_request_builder import GetRequestBuilder
