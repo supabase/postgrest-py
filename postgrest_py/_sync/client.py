d0638b4109df41495a1a892656b218e7ad675043
Dani Reinón <dani@dribo.es>
2022-01-04 18:53:10 +0100

    chore: rebuild sync

---
+++ postgrest_py/_sync/client.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from typing import Dict, cast
+
+from deprecation import deprecated
+from httpx import Response
+
+from .. import __version__
+from ..base_client import DEFAULT_POSTGREST_CLIENT_HEADERS, BasePostgrestClient
+from ..utils import SyncClient
+from .request_builder import SyncRequestBuilder
+
+
+class SyncPostgrestClient(BasePostgrestClient):
+    """PostgREST client."""
+
+    def __init__(
+        self,
+        base_url: str,
+        *,
+        schema: str = "public",
+        headers: Dict[str, str] = DEFAULT_POSTGREST_CLIENT_HEADERS,
+    ) -> None:
+        BasePostgrestClient.__init__(self, base_url, schema=schema, headers=headers)
+        self.session = cast(SyncClient, self.session)
+
+    def create_session(
+        self,
+        base_url: str,
+        headers: Dict[str, str],
+    ) -> SyncClient:
+        return SyncClient(base_url=base_url, headers=headers)
+
+    def __enter__(self) -> "SyncPostgrestClient":
+        return self
+
+    def __exit__(self, exc_type, exc, tb) -> None:
+        self.aclose()
+
+    def aclose(self) -> None:
+        self.session.aclose()
+
+    def from_(self, table: str) -> SyncRequestBuilder:
+        """Perform a table operation."""
+        base_url = str(self.session.base_url)
+        headers = dict(self.session.headers.items())
+        session = self.create_session(base_url, headers)
+        session.auth = self.session.auth
+        return SyncRequestBuilder(session, f"/{table}")
+
+    def table(self, table: str) -> SyncRequestBuilder:
+        """Alias to self.from_()."""
+        return self.from_(table)
+
+    @deprecated("0.2.0", "1.0.0", __version__, "Use self.from_() instead")
+    def from_table(self, table: str) -> SyncRequestBuilder:
+        """Alias to self.from_()."""
+        return self.from_(table)
+
+    def rpc(self, func: str, params: dict) -> Response:
+        """Perform a stored procedure call."""
+        path = f"/rpc/{func}"
+        return self.session.post(path, json=params)
---
+++ postgrest_py/_sync/request_builder.py
@@ -0,0 +1,143 @@
+from __future__ import annotations
+
+from typing import Any, Optional, Tuple
+from postgrest_py.exceptions import APIError
+
+from ..base_request_builder import (
+    APIResponse,
+    BaseFilterRequestBuilder,
+    BaseSelectRequestBuilder,
+    CountMethod,
+    pre_delete,
+    pre_insert,
+    pre_select,
+    pre_update,
+    pre_upsert,
+)
+from ..types import ReturnMethod
+from ..utils import SyncClient
+
+
+class SyncQueryRequestBuilder:
+    def __init__(
+        self,
+        session: SyncClient,
+        path: str,
+        http_method: str,
+        json: dict,
+    ) -> None:
+        self.session = session
+        self.path = path
+        self.http_method = http_method
+        self.json = json
+
+    def execute(self) -> Tuple[Any, Optional[int]]:
+        r = self.session.request(
+            self.http_method,
+            self.path,
+            json=self.json,
+        )
+        try:
+            return APIResponse.from_http_request_response(r)
+        except ValueError as e:
+            raise APIError(r.json()) from e
+
+
+class SyncFilterRequestBuilder(BaseFilterRequestBuilder, SyncQueryRequestBuilder):  # type: ignore
+    def __init__(
+        self,
+        session: SyncClient,
+        path: str,
+        http_method: str,
+        json: dict,
+    ) -> None:
+        BaseFilterRequestBuilder.__init__(self, session)
+        SyncQueryRequestBuilder.__init__(self, session, path, http_method, json)
+
+
+class SyncSelectRequestBuilder(BaseSelectRequestBuilder, SyncQueryRequestBuilder):  # type: ignore
+    def __init__(
+        self,
+        session: SyncClient,
+        path: str,
+        http_method: str,
+        json: dict,
+    ) -> None:
+        BaseSelectRequestBuilder.__init__(self, session)
+        SyncQueryRequestBuilder.__init__(self, session, path, http_method, json)
+
+
+class SyncRequestBuilder:
+    def __init__(self, session: SyncClient, path: str) -> None:
+        self.session = session
+        self.path = path
+
+    def select(
+        self,
+        *columns: str,
+        count: Optional[CountMethod] = None,
+    ) -> SyncSelectRequestBuilder:
+        method, json = pre_select(self.session, self.path, *columns, count=count)
+        return SyncSelectRequestBuilder(self.session, self.path, method, json)
+
+    def insert(
+        self,
+        json: dict,
+        *,
+        count: Optional[CountMethod] = None,
+        returning: ReturnMethod = ReturnMethod.representation,
+        upsert=False,
+    ) -> SyncQueryRequestBuilder:
+        method, json = pre_insert(
+            self.session,
+            json,
+            count=count,
+            returning=returning,
+            upsert=upsert,
+        )
+        return SyncQueryRequestBuilder(self.session, self.path, method, json)
+
+    def upsert(
+        self,
+        json: dict,
+        *,
+        count: Optional[CountMethod] = None,
+        returning: ReturnMethod = ReturnMethod.representation,
+        ignore_duplicates=False,
+    ) -> SyncQueryRequestBuilder:
+        method, json = pre_upsert(
+            self.session,
+            json,
+            count=count,
+            returning=returning,
+            ignore_duplicates=ignore_duplicates,
+        )
+        return SyncQueryRequestBuilder(self.session, self.path, method, json)
+
+    def update(
+        self,
+        json: dict,
+        *,
+        count: Optional[CountMethod] = None,
+        returning: ReturnMethod = ReturnMethod.representation,
+    ) -> SyncFilterRequestBuilder:
+        method, json = pre_update(
+            self.session,
+            json,
+            count=count,
+            returning=returning,
+        )
+        return SyncFilterRequestBuilder(self.session, self.path, method, json)
+
+    def delete(
+        self,
+        *,
+        count: Optional[CountMethod] = None,
+        returning: ReturnMethod = ReturnMethod.representation,
+    ) -> SyncFilterRequestBuilder:
+        method, json = pre_delete(
+            self.session,
+            count=count,
+            returning=returning,
+        )
+        return SyncFilterRequestBuilder(self.session, self.path, method, json)
