93d3e301e719ee479daa104f908feea947ea10de
joel@joellee.org <joel@joellee.org>
2023-01-23 13:05:43 +0800

    fix: run black and autoflake

--- postgrest/types.py
+++ postgrest/types.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from enum import Enum
-
 import sys
 
 if sys.version_info >= (3, 11):
     from enum import StrEnum
 else:
     from strenum import StrEnum
+
 
 class CountMethod(StrEnum):
     exact = "exact"
