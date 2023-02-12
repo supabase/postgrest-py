9cb711928a4f734cf70dd85c2cdf18f196c59c9a
joel@joellee.org <joel@joellee.org>
2023-02-12 16:50:17 +0800

    fix: run pre-commit hooks

--- postgrest/_sync/request_builder.py
+++ postgrest/_sync/request_builder.py
@@ -202,21 +202,19 @@ class SyncSelectRequestBuilder(BaseSelectRequestBuilder, SyncQueryRequestBuilder
             path=self.path,
             session=self.session,  # type: ignore
         )
+
     def text_search(
-            self,
-            column: str,
-            query: str,
-            options: Dict[str, any] = {}
+        self, column: str, query: str, options: Dict[str, any] = {}
     ) -> SyncFilterRequestBuilder:
-        type_ = options.get('type')
-        type_part = ''
-        if type_ == 'plain':
-            type_part = 'pl'
-        elif type_ == 'phrase':
-            type_part = 'ph'
-        elif type_ == 'web_search':
-            type_part = 'w'
-        config_part = f"{options.get('config')}" if options.get('config') else  ''
+        type_ = options.get("type")
+        type_part = ""
+        if type_ == "plain":
+            type_part = "pl"
+        elif type_ == "phrase":
+            type_part = "ph"
+        elif type_ == "web_search":
+            type_part = "w"
+        config_part = f"{options.get('config')}" if options.get("config") else ""
         self.params = self.params.add(column, f"{type_part}fts{config_part}.{query}")
 
         return SyncQueryRequestBuilder(
