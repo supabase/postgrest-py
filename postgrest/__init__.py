3ab20e4682a16b31b414fa7e5f2e1a565828f60e
Andrew Smith <a.smith@silentworks.co.uk>
2024-01-11 22:53:35 +0000

    fix: add missing RPCFilterRequestBuilder and MaybeSingleRequestBuilder exports

--- postgrest/__init__.py
+++ postgrest/__init__.py
@@ -7,16 +7,20 @@ from httpx import Timeout
 from ._async.client import AsyncPostgrestClient
 from ._async.request_builder import (
     AsyncFilterRequestBuilder,
+    AsyncMaybeSingleRequestBuilder,
     AsyncQueryRequestBuilder,
     AsyncRequestBuilder,
+    AsyncRPCFilterRequestBuilder,
     AsyncSelectRequestBuilder,
     AsyncSingleRequestBuilder,
 )
 from ._sync.client import SyncPostgrestClient
 from ._sync.request_builder import (
     SyncFilterRequestBuilder,
+    SyncMaybeSingleRequestBuilder,
     SyncQueryRequestBuilder,
     SyncRequestBuilder,
+    SyncRPCFilterRequestBuilder,
     SyncSelectRequestBuilder,
     SyncSingleRequestBuilder,
 )
