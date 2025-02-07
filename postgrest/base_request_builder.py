7446cc190a08c92ad8a85ab648db8dcff1a91642
Juan Carlos <juancarlospaco@gmail.com>
2025-02-07 17:58:27 -0300

    fix: add missing arg

--- postgrest/base_request_builder.py
+++ postgrest/base_request_builder.py
@@ -556,8 +556,9 @@ class BaseSelectRequestBuilder(BaseFilterRequestBuilder[_ReturnT]):
             if key not in ["self", "format"] and value
         ]
         options_str = "|".join(options)
+        media_type = self.headers['Accept'] if 'Accept' in self.headers else 'application/json'
         self.headers["Accept"] = (
-            f"application/vnd.pgrst.plan+{format}; options={options_str}"
+            f"application/vnd.pgrst.plan+{format}; for={media_type}; options={options_str}"
         )
         return self
 
